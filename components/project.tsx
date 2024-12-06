9c21387ec923fc7a18f8a8fd1f8be037f43a02e5
omar <omarderkaoui1337@gmail.com>
2024-12-06 21:24:12 +0100

    fix the type error

--- components/project.tsx
+++ components/project.tsx
@@ -5,7 +5,7 @@ import { projectsData } from "@/lib/data";
 import Image from "next/image";
 import { motion, useScroll, useTransform } from "framer-motion";
 
-type ProjectProps = (typeof projectsData)[number];
+type ProjectProps = (typeof projectsData)["en"][number];
 
 export default function Project({
   title,
